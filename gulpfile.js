4d2ed98f099848978bd98b4a0bdfad447111ea4b
Anshad Vattapoyil <anshad.musafir@gmail.com>
2017-08-24 21:55:58 +0530

    Initialize GulpJS task automation system

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Anshad Vattapoyil
+* @Date:   2017-08-24 21:51:48
+* @Last Modified by:   Anshad Vattapoyil
+* @Last Modified time: 2017-08-24 21:54:41
+*/
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+
+});
